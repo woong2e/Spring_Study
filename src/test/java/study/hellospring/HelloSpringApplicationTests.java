9cc23d478f1480195a7c1918fd1d2029e35c293c
woong2e <tlswodnd12345@naver.com>
2024-03-14 18:13:19 +0900

    이름 수정

--- src/test/java/study/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/study/hellospring/HelloSpringApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StudySpringApplicationTests {
+class HelloSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
