c0ec51cb347c797922d8cc47043eeeb0d02d972e
woong2e <tlswodnd12345@naver.com>
2024-03-19 23:23:38 +0900

    테스트 작성을 용이하게 하기 위한 의존성 주입

--- src/test/java/study/hellospring/service/MemberServiceTest.java
+++ src/test/java/study/hellospring/service/MemberServiceTest.java
@@ -2,22 +2,33 @@ package study.hellospring.service;
 
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import study.hellospring.domain.Member;
+import study.hellospring.repository.MemberRepository;
+import study.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class MemberServiceTest {
-    MemberService memberService  = new MemberService();
+    MemoryMemberRepository memberRepository;
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService  = new MemberService(memberRepository);
+    }
 
     @AfterEach
     public void afterEach() {
-
+        memberRepository.clearStore();
     }
 
     @Test
@@ -36,30 +47,56 @@ class MemberServiceTest {
 
     @Test
     void validateDuplicateMember() {
-        Member member = new Member();
-        member.setName("Test1");
+        //given
+        Member member1 = new Member();
+        member1.setName("Test1");
+        Member member2 = new Member();
+        member2.setName("Test1");
 
+        //when
+        memberService.join(member1);
 
+        //then
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
     @Test
     void findMembers() {
+        //given
         Member member1 = new Member();
         member1.setName("Test1");
         Member member2 = new Member();
         member2.setName("Test2");
 
+        //when
         memberService.join(member1);
         memberService.join(member2);
 
+        //then
         List<Member> testMembers = memberService.findMembers();
         for (Member member : testMembers) {
-            assertThat(member.getName())
+            assertThat(member.getName());
         }
 
     }
 
-    @Test
-    void findOne() {
-    }
+//    @Test
+//    void findOne() {
+//        //given
+//        Member member1 = new Member();
+//        member1.setName("Test1");
+//
+//        //when
+//        memberService.join(member1);
+//
+//        //then
+//        memberService.findOne(member1.getId()).get()
+//    }
 }
