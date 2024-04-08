a1f5f37e19190ebe6c2e6dd6df4fc29cc1edb0c2
woong2e <tlswodnd12345@naver.com>
2024-04-09 00:46:46 +0900

    회원 등록, 조회 req mapping

--- src/main/java/study/hellospring/controller/MemberController.java
+++ src/main/java/study/hellospring/controller/MemberController.java
@@ -2,8 +2,13 @@ package study.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.hellospring.domain.Member;
 import study.hellospring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -17,6 +22,24 @@ public class MemberController {
 
     @GetMapping("/members/new")
     public String createForm() {
-        return "member/createMemberForm";
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
