2aa1e66051e3482d678a1976d13be8080087f80a
woong2e <tlswodnd12345@naver.com>
2024-04-16 01:43:21 +0900

    feat: DB연동 MemberRepository 구현체 Spring Bean 등록

--- src/main/java/study/hellospring/SpringConfig.java
+++ src/main/java/study/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package study.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import study.hellospring.repository.JdbcMemberRepository;
 import study.hellospring.repository.MemberRepository;
 import study.hellospring.repository.MemoryMemberRepository;
 import study.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
