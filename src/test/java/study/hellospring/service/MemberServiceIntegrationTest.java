e469ba4775d05944c6f131cee9a131de4b6f6a84
woong2e <tlswodnd12345@naver.com>
2024-04-24 23:59:33 +0900

    @SpringBootTest으로  @SpringBootApplication이 있는 CoreApplication을 찾는다. @ComponentScan을 통해 빈을 스프링컨테이너에 등록 하여 테스트와 함께 실행한다. @Transactional으로 테스트 전에 트랜잭션을 시작하고, 테스트 완료 후 롤백하여 다음 테스트에 영향을 주지 않는다

---
+++ src/test/java/study/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,75 @@
+package study.hellospring.service;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.hellospring.domain.Member;
+import study.hellospring.repository.MemberRepository;
+import study.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("Test1");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member testMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(testMember.getName());
+    }
+
+    @Test
+    void validateDuplicateMember() {
+        //given
+        Member member1 = new Member();
+        member1.setName("Test1");
+        Member member2 = new Member();
+        member2.setName("Test1");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void findMembers() {
+        //given
+        Member member1 = new Member();
+        member1.setName("Test1");
+        Member member2 = new Member();
+        member2.setName("Test2");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+        List<Member> testMembers = memberService.findMembers();
+        for (Member member : testMembers) {
+            assertThat(member.getName());
+        }
+
+    }
+}
